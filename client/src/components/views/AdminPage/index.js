efbe4d6afa7c3339ec405caab5ad72c6994840de
bapibopi <harrysor@gmail.com>
2015-09-08 00:09:15 +0200

    admin prototype

--- client/src/components/views/AdminPage/index.js
+++ client/src/components/views/AdminPage/index.js
@@ -13,12 +13,24 @@ class AdminPage extends React.Component {
   render() {
     var { articles } = this.state
     return (
-      <div className="row content">
+      <div id="admin" className="row content">
         <div className="three columns">
-          <h1>Admin - Nyheter</h1>
-          menu...
+          <div className="group">
+            <h4>Hantering</h4>
+            <a className="tab">Nyheter</a>
+            <a className="tab">Konton</a>
+            <a className="tab">Tävlingar</a>
+            <a className="tab">Poll</a>
+          </div>
+          <div className="group">
+            <h4>Notiser</h4>
+            <a className="tab">Önskningar (32)</a>
+            <a className="tab">Granska profilbilder (4)</a>
+            <a className="tab">Kommentarer (6)</a>
+          </div>
         </div>
         <div className="nine columns">
+          <h3>Admin - Nyheter</h3>
           {articles.map(article => (
             <div>
               {article.title}
