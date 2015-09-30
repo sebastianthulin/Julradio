dae8729849ac907150398a829f1a7401efc3d8df
Glutch <oliverjohansson@Olivers-MacBook-Pro.local>
2015-09-30 20:29:30 +0200

    profiledesign

--- client/src/components/views/UserProfile.js
+++ client/src/components/views/UserProfile.js
@@ -4,7 +4,16 @@ class UserProfile extends React.Component {
   render() {
     return (
       <div className="row content">
-        ...
+        <div className="profileBox">
+          <div className="profPicture"></div>
+          <div className="profPM">Skicka Meddelande</div>
+          <div className="profName">Oliver Johansson</div>
+          <div className="profAge">Göteborg, 20 År</div>
+          <div className="profText">
+            <div className="profTextBox">Jag är en livs levande snigelfitta. Föddes den 29 december från mitt mammas anus - detta kan uppfattas som annorlunda och lite konstigt men redan från ung ålder valde jag att vandra min egen väg.
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
