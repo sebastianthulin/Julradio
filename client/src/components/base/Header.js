4f1dceb6eded82a9b54beb7c103b42f192c9231d
Glutch <oliverjohansson@Olivers-MacBook-Pro.local>
2015-10-12 19:57:58 +0200

    header icons

--- client/src/components/base/Header.js
+++ client/src/components/base/Header.js
@@ -24,31 +24,38 @@ class Header extends React.Component {
         <Link to="/" className="julradio">Julradio</Link>
         <div className="slogan">Nätets bästa julmusik</div>
         <div className="user">
+          <MenuItem if={user && user.admin} to="/admin/articles">
+            <i className="fa fa-shield" />
+            Admin
+          </MenuItem>
           <a href="https://webchat.quakenet.org/?channels=julradio&nick=" target="_new">
             <button>
-              <i className="fa fa-comment" />
+              <i className="fa fa-certificate" />
+              IRC
             </button>
           </a>
           <MenuItem if={user} to="/messages/">
+            <i className="fa fa-comments" />
             Meddelanden
           </MenuItem>
           <MenuItem if={user} to={user && `/@${user.username}`}>
+            <i className="fa fa-user" />
             {user && user.username}
           </MenuItem>
+          <div className="menuDivider"></div>
           <MenuItem if={user} to="/settings">
-            Inställningar
-          </MenuItem>
-          <MenuItem if={user && user.admin} to="/admin/articles">
-            Admin
+            <i className="fa fa-cog bigIco" />
           </MenuItem>
           <MenuItem if={user} onClick={UserStore.logOut}>
-            Logga ut
+            <i className="fa fa-sign-out bigIco" />
           </MenuItem>
           <MenuItem if={!user} onClick={Modal.open.bind(null, 'LogIn')}>
+            <i className="fa fa-heart" />
             Logga in
           </MenuItem>
           <MenuItem if={!user} onClick={Modal.open.bind(null, 'SignUp')}>
-            Registrera dig
+            <i className="fa fa-heart-o" />
+            Registrera
           </MenuItem>
         </div>
       </div>
