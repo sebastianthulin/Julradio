0936f6af06f474ba839d46cdb8b56c78f4520402
bapibopi <harrysor@gmail.com>
2015-11-12 00:23:43 +0100

    Volume slider & actual host picture

--- client/src/components/base/Sidebar/Player.js
+++ client/src/components/base/Sidebar/Player.js
@@ -8,26 +8,37 @@ class Player extends React.Component {
   componentWillMount() {
     RadioStore.subscribe('playing', playing => this.setState({ playing }))
     RadioStore.subscribe('currentlyPlaying', currentlyPlaying => this.setState({ currentlyPlaying }))
+    RadioStore.subscribe('volume', volume => this.setState({ volume }))
     ReservationStore.subscribe('onair', program => this.setState({ program }))
+  }
+
+  toggleMute() {
+    const { volume } = this.state
+    RadioStore.setVolume( volume ? 0: 1 )
   }
 
   render() {
     var picture
-    const { playing, currentlyPlaying, program } = this.state
+    const { playing, currentlyPlaying, program, volume } = this.state
 
     if (program) {
       picture = program.picture || program.user && program.user.picture
     }
-
     return (
       <div id="Player">
-        {picture && <img src={`/i/${picture._id + picture.extension}`} />}
+        {picture && <img src={`/picture/${picture}`} />}
         <div div className="titleControls">
-          <Link to="/history" className="songTitle">{currentlyPlaying.title}</Link>
           <div className="controls">
-            <i className={playing ? 'fa fa-pause' : 'fa fa-play'} onClick={RadioStore.toggle} />
-            <VolumeSlider />
+            <div className="item">
+              <i className={playing ? 'fa fa-pause' : 'fa fa-play'} onClick={RadioStore.toggle} />
+            </div>
+            <div className="item volumeControl">
+              <i onClick={this.toggleMute.bind(this)} className={volume == 0 ? 'fa fa-volume-off' : 'fa fa-volume-up'} />
+              <VolumeSlider />
+            </div>
+            <div className="host">{program ? program.user.name : 'Ingen värd'}</div>
           </div>
+          <Link to="/history" className="songTitle">{currentlyPlaying.title}</Link>
         </div>
       </div>
     )
--- client/src/components/base/Sidebar/VolumeSlider.js
+++ client/src/components/base/Sidebar/VolumeSlider.js
@@ -1,35 +1,41 @@
 const React = require('react')
+const cx = require('classnames')
 const RadioStore = require('../../../stores/RadioStore')
 
 class VolumeSlider extends React.Component {
   componentWillMount() {
+    this.setState({ adjusting: false })
     RadioStore.subscribe('volume', volume => this.setState({ volume }))
   }
 
   initDrag(ev) {
+    const self = this
     ev.preventDefault()
     this.handleDrag(ev)
     const handleDrag = this.handleDrag.bind(this)
     const endDrag = function() {
       document.removeEventListener('mousemove', handleDrag)
       document.removeEventListener('mouseup', endDrag)
+      self.setState({ adjusting: false })
     }
     document.addEventListener('mousemove', handleDrag)
     document.addEventListener('mouseup', endDrag)
+    this.setState({ adjusting: true })
   }
 
   handleDrag(ev) {
     const rect = this.refs.slider.getBoundingClientRect()
-    const offsetLeft = ev.clientX - rect.left
-    RadioStore.setVolume(offsetLeft / rect.width)
+    const offsetTop = ev.clientY - rect.top
+    RadioStore.setVolume(1 - offsetTop / rect.height)
   }
 
   render() {
-    const { volume } = this.state
+    const { volume, adjusting } = this.state
     return (
-      <div id="VolumeSlider" ref="slider" onMouseDown={this.initDrag.bind(this)}>
-        <div className="volume" style={{width: volume * 100 + '%'}} />
-        <div className="handle" />
+      <div id="VolumeSlider" className={cx({ adjusting })} ref="slider" onMouseDown={this.initDrag.bind(this)}>
+        <div className="volume" style={{height: volume * 100 + '%'}}>
+          <div className="handle" />
+        </div>
       </div>
     )
   }
