b1075de326e70fe9d278b265af1e5b476c444ef5
Glutch <oliverwiljohansson@gmail.com>
2016-11-22 17:32:02 +0100

    okey

--- client/src/components/views/CosyCorner.js
+++ client/src/components/views/CosyCorner.js
@@ -4,14 +4,27 @@ const Comments = require('../reusable/Comments')
 class CosyCorner extends React.Component {
   render() {
     return (
-      <div id="CosyCorner">
-        <h1>Myshörnan</h1>
-        <Comments
-          type="cosyCorner"
-          target={true}
-          placeholder="Skriv något i myshörnan"
-          signInPlaceholder="Logga in för att ta del av myset"
-        />
+      <div>
+        <div id="CosyCorner">
+          <h1>Myshörnan</h1>
+          <Comments
+            type="cosyCorner"
+            target={true}
+            placeholder="Skriv något i myshörnan"
+            signInPlaceholder="Logga in för att ta del av myset"
+          />
+        </div>
+        <div id="OnlineList">
+          <h1>Online</h1>
+          <div className="person">
+            <div className="picture"></div>
+            <div className="username">Reddan</div>
+          </div>
+          <div className="person">
+            <div className="picture"></div>
+            <div className="username">Glutch</div>
+          </div>
+        </div>
       </div>
     )
   }
